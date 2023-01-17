f101e0b7553c4245c08a8883ff7589980766141a
LizzGarleb <redscorpion3108@gmail.com>
2023-01-17 11:41:31 -0400

    pep8 validation

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for fizzbuzz in range (1,101):
+    for fizzbuzz in range(1, 101):
         if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
             print("FizzBuzz")
             continue
         elif fizzbuzz % 3 == 0:
             print("Fizz")
             continue
-        elif fizzbuzz % 5 ==  0:
+        elif fizzbuzz % 5 == 0:
             print("Buzz")
             continue
         print(fizzbuzz)
