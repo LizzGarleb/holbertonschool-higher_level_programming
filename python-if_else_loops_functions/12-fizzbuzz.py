dd8ebbcfccc1a5b29e268baacadaeadee5516579
LizzGarleb <redscorpion3108@gmail.com>
2023-01-18 10:32:21 -0400

    Task 12 correcting output

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -10,4 +10,4 @@ def fizzbuzz():
         elif fizzbuzz % 5 == 0:
             print("Buzz ", end="")
             continue
-        print(fizzbuzz + ' ', end="")
+        print(str(fizzbuzz) + ' ', end="")
