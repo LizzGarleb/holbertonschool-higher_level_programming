5d7897910db7811fa7a470fba90ccd262fdfc6da
LizzGarleb <redscorpion3108@gmail.com>
2023-01-18 10:27:12 -0400

    Task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 def fizzbuzz():
     for fizzbuzz in range(1, 101):
         if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz ", end="")
             continue
         elif fizzbuzz % 3 == 0:
-            print("Fizz")
+            print("Fizz ", end="")
             continue
         elif fizzbuzz % 5 == 0:
-            print("Buzz")
+            print("Buzz ", end="")
             continue
-        print(fizzbuzz)
+        print(fizzbuzz, ' ', end="")
