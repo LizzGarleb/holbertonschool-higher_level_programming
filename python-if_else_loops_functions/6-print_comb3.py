a44b897494e2455df4a3c1981e4a49a7dca81ca2
LizzGarleb <redscorpion3108@gmail.com>
2023-01-18 13:37:32 -0400

    Task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number in range (0, 10):
+    for i in range(0, 10):
+        if number == i:
+            continue
+        if number > i:
+            continue
+        print('{}{}'.format(number, i))
