e15780ee0dfd82c1497d5df5071bd85fd4ebc4c0
LizzGarleb <redscorpion3108@gmail.com>
2023-01-18 10:01:46 -0400

    Task 1 betty for py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,8 @@ if number <= 0:
     elif number % 10 == 0:
         print(str(number)[-1:], 'and is 0')
     elif number % 10 < 6 and number % 10 != 0:
-        print(str(number)[:1] + str(number)[-1:] + ' and is less than 6 and not 0')
+        print(str(number)[:1] + str(number)[-1:], end="")
+        print(' and is less than 6 and not 0')
 elif number > 0:
     if number % 10 > 5:
         print(str(number)[-1:], 'and is greater than 5')
