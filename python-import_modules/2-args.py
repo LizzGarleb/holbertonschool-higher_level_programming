e49abd130a506e4b7092b1b1fb6acb26aae41085
LizzGarleb <redscorpion3108@gmail.com>
2023-01-19 13:53:16 -0400

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,6 +6,6 @@ argc = len(sys.argv)
 if argc == 1:
     print("0 arguments.")
 else:
-    print("{} arguments:".format(argc - 1))
+    print("{} argument:".format(argc - 1))
     for j in range(1, argc):
         print("{}: {}".format(j, str(sys.argv[j])))
