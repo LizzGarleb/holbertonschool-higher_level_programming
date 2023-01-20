f97697906fbb71d1ced8bcc5d6bdb7eef383cb73
LizzGarleb <redscorpion3108@gmail.com>
2023-01-20 14:58:14 -0400

    file 100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import *
+
+import sys
+
+ac = len(sys.argv)
+
+if ac != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+    exit(1)
+
+ope = "+-*/"
+a = int(sys.argv[1])
+input_ope = sys.argv[2]
+b = int(sys.argv[3])
+
+if input_ope == '+':
+    result = a + b
+    print("{} {} {} = {}".format(a, input_ope, b, result))
+elif input_ope == '-':
+    result = a - b
+    print("{} {} {} = {}".format(a, input_ope, b, result))
+elif input_ope == '*':
+    result = a * b
+    print("{} {} {} = {}".format(a, input_ope, b, result))
+elif input_ope == '/':
+    result = a / b
+    print("{} {} {} = {}".format(a, input_ope, b, result))
+else:
+    print("Unkown operator. Available operators: +, -, * and /\n")
+    exit(1)
