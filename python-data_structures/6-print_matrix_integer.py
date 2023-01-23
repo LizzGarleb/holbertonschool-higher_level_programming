e82b557fbcb4cf0bf956aa5eed806291aef02bc8
LizzGarleb <redscorpion3108@gmail.com>
2023-01-23 13:23:38 -0400

    task 6 validated

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
         print('')
-    for i in range(len(matrix)): #find how many row it have
-        for j in range(len(matrix[i])): #find the coloums
-            if j == len(matrix[i]) - 1: #mark the ending of the matrix
+    for i in range(len(matrix)):  # find how many row it have
+        for j in range(len(matrix[i])):  # find the coloums
+            if j == len(matrix[i]) - 1:  # mark the ending of the matrix
                 print("{:d}".format(matrix[i][j]))
-            else: #print next to it
+            else:  # print next to it
                 print("{:d} ".format(matrix[i][j]), end="")
