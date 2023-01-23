b844dd0b941a0f97e02a8f61fe32fa5064df6cc0
LizzGarleb <redscorpion3108@gmail.com>
2023-01-23 12:21:35 -0400

    Task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = my_string.tranlate({ord('cC'):None})
-    return(new_str)
+    new_str = my_string.tranlate({ord(i): None for i in 'Cc'})
+    return new_str
