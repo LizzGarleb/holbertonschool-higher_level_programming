a0e1da9c0fd3a00b7ca7bb7aea675eabe3de85ca
LizzGarleb <redscorpion3108@gmail.com>
2023-01-23 10:43:07 -0400

    Adding str.format

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):
         return(None)
     if idx > len(my_list):
         return(None)
-    print("Element at index {:d} is {}".format(idx, my_list[idx]))
+    print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
