1f57855950643124390a88fa4991570fb99b0706
LizzGarleb <redscorpion3108@gmail.com>
2023-01-23 16:12:40 -0400

    list copy

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list
+    new_list = my_list.copy()
     for i in range(len(my_list)):
         if new_list[i] % 2 == 0:
             new_list[i] = True
