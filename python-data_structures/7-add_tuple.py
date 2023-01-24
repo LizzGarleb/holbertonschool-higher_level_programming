809c2ab2cd3be4a14e0152f7be392e5398f2a2c0
LizzGarleb <redscorpion3108@gmail.com>
2023-01-23 20:14:22 -0400

    Task 7 adding lenght to first if

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a[0] < 2:
+    if len(tuple_a) < 2:
         tuple_a += (0, 0)
     if len(tuple_b) < 2:
         tuple_b += (0, 0)
