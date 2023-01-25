3115452285a831fac9fd04d426eb948cd14e6f22
LizzGarleb <redscorpion3108@gmail.com>
2023-01-25 17:23:41 -0400

    Task 12 pep8validation

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (type(roman_string) == str) == False or roman_string == None:
+    if (type(roman_string) == str) is False or roman_string is None:
         return 0
-    roman_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rom_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     output = 0
     for letters in roman_string:
-        output += roman_num[letters]
+        output += rom_num[letters]
     return(output)
