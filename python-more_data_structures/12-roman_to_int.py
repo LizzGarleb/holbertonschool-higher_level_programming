1c30ebd6024ef8f00e8023ff24aaecc5b4fc1fa0
LizzGarleb <redscorpion3108@gmail.com>
2023-01-25 17:11:44 -0400

    Task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (type(roman_string) == str) == False or roman_string == None:
+        return 0
+    roman_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    output = 0
+    for letters in roman_string:
+        output += roman_num[letters]
+    return(output)
