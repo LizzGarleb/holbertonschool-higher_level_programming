2e5b334ec9db8714056b29b1ecf5d138f4705609
LizzGarleb <redscorpion3108@gmail.com>
2023-01-27 11:11:12 -0400

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
         for i in range(x):
-            if isinstance(my_list[i], int) == True:
+            if isinstance(my_list[i], int) is True:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         print()
