23100bf9eef50d735c53e462b3f8456f5dd0bf06
LizzGarleb <redscorpion3108@gmail.com>
2023-01-30 10:07:16 -0400

    typo in type error messague

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """Definition of square attribute"""
     def __init__(self, size=0):
         if type(size) is not int:
-            raise TypeError('size must be an interger')
+            raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
