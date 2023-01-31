c7f0c36547299ba22fea47e349e442a86477a2ee
LizzGarleb <redscorpion3108@gmail.com>
2023-01-31 10:03:08 -0400

    Task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -38,7 +38,7 @@ class Square:
         """set current position"""
         if(len(value) < 2 or type(value[0]) is not int or
             type(value[1]) is not int or value[0] < 0 or
-             value[1] < 0):
+                value[1] < 0):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
