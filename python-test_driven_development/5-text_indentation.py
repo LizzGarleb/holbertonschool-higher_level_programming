b64cb9e8557e7f58f3f27a81aef0683f1639a6bd
LizzGarleb <redscorpion3108@gmail.com>
2023-02-02 15:03:31 -0400

    Task 4 module and function description

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+    This module will have a
+    function that will print
+    a text with 2 new lines after
+    each of these characters:
+    ". ? :"
+"""
 def text_indentation(text):
+    """
+    Function that prints a text
+    with 2 new lines after each
+    of the chracters above.
+    First we check that what its
+    passed is a str, then proceed to check and print.
+    """
     if type(text) is not str:
         raise TypeError('text must be a string')
     for let in range(len(text)):
