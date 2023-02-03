7cae085358249b0bc23eec10e520bd4f8efdc88c
LizzGarleb <redscorpion3108@gmail.com>
2023-02-03 19:30:00 -0400

    static method added

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -93,6 +93,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if isinstance(rect_1, Rectangle) is False:
             raise TypeError('rect_1 must be an instance of Rectangle')
