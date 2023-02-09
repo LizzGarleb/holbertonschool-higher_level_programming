ee0dc055f784eca2db159b66341ecb34c284886d
LizzGarleb <redscorpion3108@gmail.com>
2023-02-08 22:16:55 -0400

    Task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module have a empty class named Geometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle is a child class from BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
