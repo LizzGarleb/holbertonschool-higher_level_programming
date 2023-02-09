24e4a865245ee379ab82fe5e97a55f53c6be4f89
LizzGarleb <redscorpion3108@gmail.com>
2023-02-08 22:19:32 -0400

    module documentation ??

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,6 +9,9 @@ class Rectangle(BaseGeometry):
     Rectangle is a child class from BaseGeometry
     """
     def __init__(self, width, height):
+        """
+        Function that validates the weight & height
+        """
         self.__width = width
         self.__height = height
         super().integer_validator("width", width)
