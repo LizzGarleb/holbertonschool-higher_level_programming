205ef3c40b31fe427b4453530de306f0542e9dbd
LizzGarleb <redscorpion3108@gmail.com>
2023-02-08 22:22:47 -0400

    task 8 added import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,6 +5,9 @@ is a child of BaseGeometry
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """
     Rectangle is a child class from BaseGeometry
