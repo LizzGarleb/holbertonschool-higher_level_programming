894c4e2d7c08b62ee6d45b0de8c46047db28cf83
LizzGarleb <redscorpion3108@gmail.com>
2023-02-10 14:59:23 -0400

    Task 2 documentation

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module have a function named append_write
+that appends the text given to the file
+"""
+
+
 def append_write(filename="", text=""):
     """
     This function appends a tring at the
