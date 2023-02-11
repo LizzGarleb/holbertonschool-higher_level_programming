059548d690c35170413dafa53a94bd57e443f532
LizzGarleb <redscorpion3108@gmail.com>
2023-02-10 20:41:12 -0400

    error with passed arguments

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_json_file(my_obj, filename):
     text file, using JSON representation
     """
     with open(filename, 'w') as file:
-        json.dump(my_obj, filename)
+        json.dump(my_obj, file)
