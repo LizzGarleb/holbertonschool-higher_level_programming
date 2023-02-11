bfbe39ef3201abdf1c391f8a6c4dc613e7b82922
LizzGarleb <redscorpion3108@gmail.com>
2023-02-10 20:56:26 -0400

    Task 9 pep 8 validation

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -9,9 +9,10 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """
         Retrieves a dictionary representation
         of the Student class
         """
-        return  self.__dict__
+        return self.__dict__
