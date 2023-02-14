d2240312a8ed77a68e9f4e551426888f7a7be29d
LizzGarleb <redscorpion3108@gmail.com>
2023-02-14 15:02:24 -0400

    maybe x3??

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,6 +33,6 @@ class Base:
         the dictionary passed to us
         """
         if list_dictionaries is None:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
