9514df022965ee1dbf1cef1387d458ff8b17077c
LizzGarleb <redscorpion3108@gmail.com>
2023-02-14 11:08:45 -0400

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -31,3 +31,30 @@ class Square(Rectangle):
     @size.setter
     def size(self, size):
         self.width = size
+
+    def update(self, *args, **kwargs):
+
+        if args is not None:
+            count = 0
+            for arg in args:
+                if count == 0:
+                    self.id = arg
+                elif count == 1:
+                    self.width = arg
+                elif count == 2:
+                    self.height = arg
+                elif count == 3:
+                    self.x = arg
+                elif count == 4:
+                    self.y = arg
+                count += 1
+
+        for key, elem in kwargs.items():
+            if key == "id":
+                self.id = elem
+            if key == "size":
+                self.width = elem
+            if key == "x":
+                self.x = elem
+            if key == "y":
+                self.y = elem
