c77b3a71e06304299b4d5402ffdf3895325da9cf
LizzGarleb <redscorpion3108@gmail.com>
2023-02-14 10:19:07 -0400

    changed init if statement

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -19,22 +19,6 @@ class Square(Rectangle):
         proceed to assign them.
         """
         super().__init__(size, size, x, y, id)
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        elif type(x) is not int:
-            raise TypeError('x must be an integer')
-        elif type(y) is not int:
-            raise TypeError('y must be an integer')
-        elif size <= 0:
-            raise ValueError('size must be > 0')
-        elif x < 0:
-            raise ValueError('x must be >= 0')
-        elif y < 0:
-            raise ValueError('y must be >= 0')
-        else:
-            self.size = size
-            self.x = x
-            self.y = y
 
     def __str__(self):
         """ Returns [Square] (id) x/y - size """
