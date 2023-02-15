4272de1f5b3c2db03bbc36e43c6ed462ecf56182
LizzGarleb <redscorpion3108@gmail.com>
2023-02-15 14:33:58 -0400

    testing rectangle edge cases

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -5,7 +5,6 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
 
     """ Testing width conditions """
-
     def test_width(self):
         rectangle = Rectangle(2, 4)
         self.assertEqual(rectangle.width, 2)
@@ -23,7 +22,6 @@ class TestRectangle(unittest.TestCase):
             rec = Rectangle("2", 10)
 
     """ Testing height conditions """
-
     def test_height(self):
         rectangle = Rectangle(2, 4)
         self.assertEqual(rectangle.height, 4)
@@ -45,8 +43,44 @@ class TestRectangle(unittest.TestCase):
         rectangle = Rectangle(10, 2, 3, 5)
         self.assertEqual(rectangle.x, 3)
 
-    """ Testing area """
+    def test_x_negative(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(5, 5, -9)
 
+    def test_x_zero(self):
+        rectangle = Rectangle(3, 2, 0, 1)
+        self.assertEqual(rectangle.x, 0)
+
+    def test_x_str(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(5, 5, "1")
+
+    """ Testing y conditions """
+    def test_y(self):
+        rectangle = Rectangle(2, 3, 2, 2)
+        self.assertEqual(rectangle.y, 2)
+
+    def test_y_negative(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(10, 2, 3, -1)
+
+    def test_y_zero(self):
+        rectangle = Rectangle(3, 2, 1, 0)
+        self.assertEqual(rectangle.y, 0)
+
+    def test_y_str(self):
+        with self.assertRaises(TypeError):
+            rec = Rectangle(4, 6, 2, "2")
+
+    """ Testing area """
     def test_area(self):
         rectangle = Rectangle(3, 3)
         self.assertEqual(rectangle.area(), 9)
+
+    def test_area_2(self):
+        rec = Rectangle(2, 10)
+        self.assertEqual(rec.area(), 20)
+
+    def test_area_3(self):
+        rec = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(rec.area(), 56)
