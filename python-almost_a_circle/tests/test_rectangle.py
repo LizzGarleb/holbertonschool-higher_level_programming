b3db7f7a20a1929602e961429948b3fd856dc5ca
LizzGarleb <redscorpion3108@gmail.com>
2023-02-15 22:04:19 -0400

    rectangle

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -1,7 +1,6 @@
 import unittest
-from io import StringIO
+import io
 import unittest.mock
-from unittest.mock import patch
 
 from models.rectangle import Rectangle
 
@@ -89,6 +88,16 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rec.area(), 56)
 
     """ Testing display """
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def assert_stdout(self, expected_output, w, h, x, y, mock_stdout):
+        rec = Rectangle(w, h, x, y)
+        rec.display()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_display(self):
+        self.assert_stdout("\n ##\n ##\n", 2, 2, 1, 1)
+        self.assert_stdout("\n\n  ##\n  ##\n  ##\n", 2, 3, 2, 2)
+        self.assert_stdout(" ###\n ###\n", 3, 2, 1, 0)
 
     """ Testing __str__ """
     def test_str_method(self):
@@ -111,6 +120,6 @@ class TestRectangle(unittest.TestCase):
 
     """  Testing to_dictionary method """
     def test_to_dictionary(self):
-        rec = Rectangle(10, 2, 1, 9)
-        result = {'x': 1, 'y': 9, 'id': 8, 'height': 2, 'width': 10}
+        rec = Rectangle(10, 2, 1, 9, 7)
+        result = {'x': 1, 'y': 9, 'id': 7, 'height': 2, 'width': 10}
         self.assertDictEqual(rec.to_dictionary(), result)
