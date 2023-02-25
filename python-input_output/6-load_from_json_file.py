79f39aa55d0e55222befe7d644310032610fdb23
LizzGarleb <redscorpion3108@gmail.com>
2023-02-25 09:44:53 -0400

    pep 8 validated

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import json
+"""
+Function that creates an Object
+from a JSON file
+"""
+
+
 def load_from_json_file(filename):
+    """Creates an Object from a JSON file """
     with open(filename, 'r') as fd:
         return json.loads(fd.read())
-
