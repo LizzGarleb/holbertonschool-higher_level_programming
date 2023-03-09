6a9c47c3b8372ae8d1cdbb74660bd320ffd6f669
LizzGarleb <redscorpion3108@gmail.com>
2023-03-09 11:05:16 -0400

    task 16 no values

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script that list all records of the table
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
