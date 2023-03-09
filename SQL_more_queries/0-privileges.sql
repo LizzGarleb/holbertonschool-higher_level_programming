f90f79b705aaa6b7bde92a4c98e9c914d2e99644
LizzGarleb <redscorpion3108@gmail.com>
2023-03-09 14:28:42 -0400

    seperate them instead of a one linear

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- Script that lists all privileges of the MySQL user on your server
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@"localhost";
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOT 'user_0d_2'@'localhost';
