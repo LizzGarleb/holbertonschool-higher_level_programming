e4e8c7d7e18f55acbea731d35b9ac849b19d43e7
LizzGarleb <redscorpion3108@gmail.com>
2023-03-10 12:08:24 -0400

    typo

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Script that list all shows
-SELECT tv_shows.title, tv_shows_genres.genre_id FROM titles JOIN genre_id ON tv_shows.title=tv_shows_genres.genre_id ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+SELECT tv_shows.title, tv_shows_genres.genre_id FROM title JOIN genre_id ON tv_shows.title=tv_shows_genres.genre_id ORDER BY tv_shows.title, tv_shows_genres.genre_id;
