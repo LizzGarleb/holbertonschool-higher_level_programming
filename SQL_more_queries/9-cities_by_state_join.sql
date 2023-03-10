1cda733d99e318c68ddb5532c3f7cb7d17d3c64a
LizzGarleb <redscorpion3108@gmail.com>
2023-03-10 10:51:53 -0400

    this will get rid of the double

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Script that list all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = state_id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id;
