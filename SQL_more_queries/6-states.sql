89efcdcab9585b64fff5103968f9516514ee4749
LizzGarleb <redscorpion3108@gmail.com>
2023-03-09 23:00:48 -0400

    MISSING IF STATEMENT

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Script that creates a database and a table
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL;
