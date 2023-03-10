1d10e6b6432050d11e46b0ca0b347839a6ec1aa9
LizzGarleb <redscorpion3108@gmail.com>
2023-03-10 10:25:04 -0400

    task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Script that lists all the cities of Cali that can be found
-SELECT id, name FROM cities WHERE state_id.name=California ORDER BY cities.id;
+SELECT id, name FROM cities WHERE state_id name=California ORDER BY cities.id;
