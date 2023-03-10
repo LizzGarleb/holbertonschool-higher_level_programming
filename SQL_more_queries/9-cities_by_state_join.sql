c2093e8b888d25c91a2d812e5411fe71042f2c5a
LizzGarleb <redscorpion3108@gmail.com>
2023-03-10 10:37:55 -0400

    comma seperating

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Script that list all cities contained in the database
-SELECT cities.id cities.name states.name FROM hbtn_0d_usa ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa ORDER BY cities.id;
