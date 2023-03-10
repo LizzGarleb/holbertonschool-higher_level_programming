5f7b1e9cb248002430b988f390afd581e6fa8b0e
LizzGarleb <redscorpion3108@gmail.com>
2023-03-10 10:50:29 -0400

    maybe??

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Script that list all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa JOIN states ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = state_id ORDER BY cities.id;
