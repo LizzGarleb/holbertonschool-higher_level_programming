e014b330c7747c486fb128e56c5c0d3ce5bf5093
LizzGarleb <redscorpion3108@gmail.com>
2023-03-09 23:10:20 -0400

    typo at naming the database

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- Script that creates databases and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
