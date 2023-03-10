36605171d6bc0ceccb40a10afcfb1742c800ad0d
LizzGarleb <redscorpion3108@gmail.com>
2023-03-10 12:12:22 -0400

    trying to combine them by id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Script that list all shows
-SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows JOIN tv_shows_genres ON tv_shows.title=tv_shows_genres.genre_id ORDER BY tv_shows.title, tv_shows_genres.genre_id;
+SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows JOIN tv_shows_genres ON tv_shows.id=tv_shows_genres.show_id ORDER BY tv_shows.title, tv_shows_genres.genre_id;
