64954215f5320319071c04e9dc40b4d24f0f033f
LizzGarleb <redscorpion3108@gmail.com>
2023-03-10 21:44:28 -0400

    left join added

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- Script that uses a database to list all genres of a show
-SELECT tv_genres.name FROM tv_shows JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
+SELECT tv_genres.name FROM tv_shows LEFT JOIN tv_shows ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
