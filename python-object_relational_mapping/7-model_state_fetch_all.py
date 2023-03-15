3aa5265fecc74a0642f2c43dad5114d112b45f68
LizzGarleb <redscorpion3108@gmail.com>
2023-03-15 11:34:46 -0400

    pycodestyle

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == '__main__':
     username = argv[1]
     psw = argv[2]
