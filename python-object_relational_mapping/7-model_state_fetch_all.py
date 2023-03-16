846537f651a4087fc6309a803a2c014a612fa872
LizzGarleb <redscorpion3108@gmail.com>
2023-03-16 12:10:58 -0400

    removing the all

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all()
+    for state in session.query(State).order_by(State.id)
         print(f"{states.id}: {states.name}")
 
     session.close()
