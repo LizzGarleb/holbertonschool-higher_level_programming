12cb9e18314f0e4c1fd33df09036e8fbb63c2847
LizzGarleb <redscorpion3108@gmail.com>
2023-03-16 16:00:52 -0400

    task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter_by(name=name)
+    states = session.query(State).order_by(State.id).filter(State.name == name).all()
     if states:
         print(states.id)
     else:
