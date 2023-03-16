c24d49d82d6d37d45fc534bf2e4469d10aef9ad0
LizzGarleb <redscorpion3108@gmail.com>
2023-03-16 11:36:17 -0400

    task 7 documentation now?

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Lists all states object from the database
-"""
+"""Lists all states object from the database"""
 
 
 from sys import argv
@@ -11,12 +9,15 @@ from model_state import Base, State
 
 
 if __name__ == '__main__':
-    url = "mysql+mysqlconnector://{user}:{password}@localhost:3306/{database}"
-    engine = create_engine(url.format(
-        user=argv[1]
-        password=argv[2]
-        database=argv[3]
-    ))
+    """ Lists all state object from the database """
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine(
+        "mysql+mysqlconnector://{user}:{password}@localhost:3306/{database}"
+        )
 
     Session = sessionmaker(bind=engine)
     session = Session()
