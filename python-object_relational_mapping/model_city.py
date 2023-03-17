6a1fc4aa142bf35250c7eb866cd2c982929810d0
LizzGarleb <redscorpion3108@gmail.com>
2023-03-16 22:15:10 -0400

    correction of class name

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -11,9 +11,9 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """ State class definition """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
 
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
