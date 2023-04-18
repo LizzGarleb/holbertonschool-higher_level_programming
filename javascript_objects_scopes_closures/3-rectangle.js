cc61a801f3837360a372022674576b22461c16ae
LizzGarleb <redscorpion3108@gmail.com>
2023-04-18 14:41:27 -0400

    task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-            // empty object
-        } else {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      // empty object
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    print () {
-        let rect_print = ''
-        for (let i = 0; i < this.width; i++) {
-            rect_print += 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
-            console.log(rect_print);
-        }
+  }
+
+  print () {
+    let rect = '';
+    for (let i = 0; i < this.width; i++) {
+      rect += 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(rect);
+    }
+  }
 }
 
 module.exports = Rectangle;
