5e5ed557fdcec8d9fe3601764d6138c855a83d02
LizzGarleb <redscorpion3108@gmail.com>
2023-04-18 14:49:51 -0400

    task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      // empty object
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rect = '';
+    for (let i = 0; i < this.width; i++) {
+      rect += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
