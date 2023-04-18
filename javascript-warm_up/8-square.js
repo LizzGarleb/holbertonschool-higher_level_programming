626d520a4f00b3954740e63a4def4804ae088b60
LizzGarleb <redscorpion3108@gmail.com>
2023-04-18 09:17:26 -0400

    task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const num = process.argv[2];
+
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  let letter = 'X';
+  for (let i = 0; i < num; i++) {
+    letter += 'X';
+  }
+  for (let i = 0; i < num; i++) {
+    console.log(letter);
+  }
+}
