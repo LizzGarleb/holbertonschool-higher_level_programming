4a8b243c881e34a66281715995000b914962f7ac
LizzGarleb <redscorpion3108@gmail.com>
2023-04-21 08:37:33 -0400

    fix task 4 done

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[1];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const url = process.argv[2];
 
-request(url, (err, rest, body) => {
+request(url, (err, response, body) => {
   if (err) throw err;
   else {
     const response = JSON.parse(body);
     const films = response.results;
     let count = 0;
-    for (let i = 0; i < films.lenght; i++) {
+    for (let i = 0; i < films.length; i++) {
       for (let j = 0; j < films[i].characters.length; j++) {
         if (films[i].characters[j].includes('18')) {
           count++;
