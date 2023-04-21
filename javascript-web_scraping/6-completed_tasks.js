2d646a8fd834d1f72396aa60578df84732395090
LizzGarleb <redscorpion3108@gmail.com>
2023-04-21 08:51:34 -0400

    task 6 done

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, response, body) => {
+  if (err) throw err;
+  else {
+    const tasks = JSON.parse(body);
+    const allTasks = {};
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed === true) {
+        if (allTasks[tasks[i].userId] === undefined) {
+          allTasks[tasks[i].userId] = 1;
+        } else {
+          allTasks[tasks[i].userId]++;
+        }
+      }
+    }
+    console.log(allTasks);
+  }
+});
