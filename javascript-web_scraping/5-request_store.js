f731c45515a0f54ba725d58aa8e6f50ed8833335
LizzGarleb <redscorpion3108@gmail.com>
2023-04-21 08:41:21 -0400

    task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+request(url, (err, response, body) => {
+  if (err) throw err;
+  else {
+    const content = body;
+    fs.writeFile(filePath, content, 'utf-8', (err) => {
+      if (err) throw err;
+    });
+  }
+});
